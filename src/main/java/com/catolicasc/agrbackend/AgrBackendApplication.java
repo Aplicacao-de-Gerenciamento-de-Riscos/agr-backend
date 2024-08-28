42d0a921f8b978f23562e70428093c69161b7ce3
Brenda Aldrovandi Gaio <bbrendagaioo@gmail.com>
2024-08-28 19:33:08 -0300

    First Commit

---
+++ src/main/java/com/catolicasc/agrbackend/AgrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.catolicasc.agrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/catolicasc/agrbackend/AgrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catolicasc.agrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
